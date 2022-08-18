cbcb89884f6bac31415866e266d178c5aaa8584e
Jake Hemmerle <jakehemmerle@protonmail.com>
2022-08-18 02:00:13 -0400

    basic map with mapbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Map from 'react-map-gl';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Map
+    initialViewState={{
+      longitude: -100,
+      latitude: 40,
+      zoom: 3.5
+    }}
+    style={{ width: 600, height: 400 }}
+    mapStyle="mapbox://styles/mapbox/streets-v9"
+  />;
 }
 
 export default App;
