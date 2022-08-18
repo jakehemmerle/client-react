3be39bac5bd6d263d32d637f156e195641200f98
Jake Hemmerle <jakehemmerle@protonmail.com>
2022-08-18 03:52:14 -0400

    still sucks but less

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 This frontend is made with Create React App and is using mapbox.com under the hood.
 
 Sign up on mapbox.com to get an API token to run this.
+
+## Resources
+
+<https://www.mapbox.com/impact-tools/finder>
--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import Map from 'react-map-gl';
 function App() {
   return <Map
     initialViewState={{
-      longitude: -100,
-      latitude: 40,
-      zoom: 3.5
+      latitude: 39.140115,
+      longitude: -84.513876,
+      zoom: 8
     }}
-    style={{ width: 600, height: 400 }}
-    mapStyle="mapbox://styles/mapbox/streets-v9"
+    style={{ width: 1260, height: 720 }}
+    mapStyle="mapbox://styles/mapbox/streets-v10"
   />;
 }
 
